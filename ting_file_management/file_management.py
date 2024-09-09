22847667fb92446560cc9d67321fc46fdbbf4246
CamilaGon <cg561351@gmail.com>
2024-09-09 19:35:09 -0300

    req 2 e 3

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.lower().endswith('.txt'):
+        print("Formato inválido", file=sys.stderr)
+        return []
+    try:
+        with open(path_file, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            return [line.strip() for line in lines]
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return []
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,30 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+
+    lines = txt_importer(path_file)
+    if not lines:
+        return
+    for item in instance.items:
+        if item['nome_do_arquivo'] == path_file:
+            return
+    file_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+    instance.enqueue(file_data)
+    print(file_data)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not instance.is_empty():
+        instance.dequeue()
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position < 0 or position >= len(instance):
+        raise IndexError("Índice Inválido ou Inexistente")
+
+    return instance.items[position]
