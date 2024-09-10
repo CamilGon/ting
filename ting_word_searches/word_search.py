5d2d4bdf4874d629bfe82da7d116a5e10674e581
CamilaGon <cg561351@gmail.com>
2024-09-09 21:32:10 -0300

    req 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+    word_lower = word.lower()
+    for file_info in instance.items:
+        lines = file_info.get("linhas_do_arquivo", [])
+        ocorrencias = []
+        for index, line in enumerate(lines):
+            if word_lower in line.lower():
+                ocorrencias.append({"linha": index + 1})
+        if ocorrencias:
+            results.append({
+                "palavra": word,
+                "arquivo": file_info.get("nome_do_arquivo"),
+                "ocorrencias": ocorrencias
+            })
+    return results
 
 
 def search_by_word(word, instance):
